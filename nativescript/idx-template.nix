7694d131285b7ec770ee81031e7f26c492409093
farhan.aboobacker <farhan.aboobacker@cognizant.com>
2025-12-04 06:11:29 +0000

    nodejsversion updated

--- nativescript/idx-template.nix
+++ nativescript/idx-template.nix
@@ -31,8 +31,9 @@
  */
 
 { pkgs, template ? "js", ts ? false, ... }: {
+  channel = "stable-25.05"
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs
     pkgs.python3
     pkgs.python311Packages.pip
     pkgs.python311Packages.fastapi
@@ -43,12 +44,12 @@
     mkdir -p "$out/.idx/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
     shopt -s dotglob; cp -r ${./dev}/* "$out"
-    npm install nativescript@8.6.1
+    npm install nativescript
     ./node_modules/nativescript/bin/ns create example --${template} ${if ts then "--ts" else ""} --path "$out"
     mv "$out/example"/* "$out/"
     rmdir "$out/example"
     chmod -R +w "$out"
-    cd "$out"; npm install -D nativescript@8.6.1
+    cd "$out"; npm install -D nativescript
     cd "$out"; npm install --package-lock-only --ignore-scripts
   '';
 }
