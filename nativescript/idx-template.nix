0127de075399f9c2bc8153ffe72bfa987405c71c
farhan.aboobacker <farhan.aboobacker@cognizant.com>
2025-12-04 10:13:59 +0000

    version

--- nativescript/idx-template.nix
+++ nativescript/idx-template.nix
@@ -44,12 +44,12 @@
     mkdir -p "$out/.idx/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
     shopt -s dotglob; cp -r ${./dev}/* "$out"
-    npm install nativescript
+    npm install nativescript@8.5.2
     ./node_modules/nativescript/bin/ns create example --${template} ${if ts then "--ts" else ""} --path "$out"
     mv "$out/example"/* "$out/"
     rmdir "$out/example"
     chmod -R +w "$out"
-    cd "$out"; npm install -D nativescript
+    cd "$out"; npm install -D nativescript@8.5.2
     cd "$out"; npm install --package-lock-only --ignore-scripts
   '';
 }
