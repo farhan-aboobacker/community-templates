2b8baf4bba7be5751c17ca0c49a09dd3446e448c
farhan.aboobacker <farhan.aboobacker@cognizant.com>
2025-12-04 11:45:35 +0000

    test

--- nativescript/idx-template.nix
+++ nativescript/idx-template.nix
@@ -48,9 +48,9 @@
     mv "$WS_NAME" "$out"
 
     mkdir -p "$out/.idx"
-    chmod -R u+w "$out"
-    cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
-    cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
+    # chmod -R u+w "$out"
+    # cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
+    # cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
     chmod -R u+w "$out"
      cd "$out"; npm install -D nativescript@8
     cd "$out"; npm install --package-lock-only --ignore-scripts
