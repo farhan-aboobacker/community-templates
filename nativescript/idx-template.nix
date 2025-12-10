89ec21004f1e1dce57c3eaad5d1d06031345d750
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-12-10 13:02:24 +0530

    Update idx-template.nix

--- nativescript/idx-template.nix
+++ nativescript/idx-template.nix
@@ -14,43 +14,29 @@
  limitations under the License.
  */
 
-/*
- Copyright 2024 Google LLC
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
 { pkgs, template ? "js", ts ? false, ... }: {
-  channel = "stable-24.05";
   packages = [
     pkgs.nodejs
   ];
-bootstrap = ''    
+  bootstrap = ''    
     mkdir "$out"
     mkdir -p "$out/.idx/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
     shopt -s dotglob; cp -r ${./dev}/* "$out"
+
     if [ "${template}" = "svelte" ]; then
        npm config set legacy-peer-deps true
        npm install --save-dev nativescript
-       npx ns create example --svelte --path "$out"
+       npx ns create example --template @nativescript/template-blank-svelte --path "$out"
     else
-       npx ns create example --${template} ${if ts then "--ts" else ""} --path "$out"
+       npm install nativescript
+       ./node_modules/nativescript/bin/ns create example --${template} ${if ts then "--ts" else ""} --path "$out"
     fi
     mv "$out/example"/* "$out/"
     rmdir "$out/example"
     chmod -R +w "$out"
+    cd "$out"; npm install -D nativescript
     cd "$out"; npm install --package-lock-only --ignore-scripts
-    ''; 
-
+    
+  '';
 }
